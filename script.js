bb6f2e740f679a7045263c54b8f1fab78379f66b
sreedev <xreedev@gmail.com>
2024-07-24 17:29:15 +0530

    csv parsing done

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
-class event{
-    constructor(eventId,eventName,startDate,endDate){
-      this.eventName=eventName;
-      this.eventId=eventId;
-      this.startDate=startDate;
-      this.endDate=endDate;
-    }
+class Event {
+  constructor(eventId, eventName, startDate, endDate) {
+    this.eventName = eventName;
+    this.eventId = eventId;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
 }
 
-function submitCsv(){
-    let csvType=document.getElementById("csv-type-select").value;
-    let csvFile=document.getElementById("input-file").files[0];
-    if (csvFile) {
-        const reader = new FileReader();
-
-        reader.onload = function(e) {
-            const contents = e.target.result;
-            // You can parse the contents here, for example:
-            console.log(contents);
-        };
-
-        reader.readAsText(csvFile);
-    } else {
-        alert('No file selected');
-    }
+class Task {
+  constructor(eventId, taskName) {
+    this.eventId = eventId;
+    this.taskName = taskName;
+  }
 }
 
-function parseToEvent(contents){
-    const rows = contents.split('\n');
-    rows.forEach(row => {
-        const columns = row.split(',');
-        console.log(columns);
-        // Process each column as needed
-    });
+function submitCsv() {
+  let csvType = document.getElementById("csv-type-select").value;
+  let csvFile = document.getElementById("input-file").files[0];
+  localStorage.setItem("csvFile", csvFile);
+  if (csvFile) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const contents = e.target.result;
+      console.log(contents);
+      if (csvType == "events") {
+        parseToEvent(contents);
+      } else {
+        parseToTask(contents);
+      }
+    };
+
+    reader.readAsText(csvFile);
+  } else {
+    alert("No file selected");
+  }
+}
+
+function parseToEvent(contents) {
+  let eventList = [];
+  const rows = contents.split("\n");
+  rows.forEach((row) => {
+    const columns = row.split(",");
+    const event = new Event(columns[0], columns[1], columns[2], columns[3]);
+    eventList.push(event);
+  });
+  console.log(eventList);
+  localStorage.setItem("events", eventList);
+  return eventList;
+}
+
+function parseToTask(contents) {
+  let taskList = [];
+  const rows = contents.split("\n");
+  rows.forEach((row) => {
+    const columns = row.split(",");
+    const event = new Task(columns[0], columns[1]);
+    taskList.push(event);
+  });
+  console.log(eventList);
+  localStorage.setItem("tasks", taskList);
+  return taskList;
 }
