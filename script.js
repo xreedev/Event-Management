3ad0c22716f5a6f13b7d9e63e34a727be5546c9a
sreedev <xreedev@gmail.com>
2024-07-24 18:30:33 +0530

    Header validations done

--- script.js
+++ script.js
@@ -23,15 +23,16 @@ function submitCsv() {
 
     reader.onload = function (e) {
       const contents = e.target.result;
-      console.log(contents);
       if (csvType == "events") {
-        parseToEvent(contents);
+        if(parseToEvent(contents)){alert("Events added successfully")};
       } else {
-        parseToTask(contents);
+        if(parseToTask(contents)){alert("Tasks added successfully")};
       }
     };
 
     reader.readAsText(csvFile);
+  } else if (csvFile == "" || csvFile == null) {
+    alert("Submit a file");
   } else {
     alert("No file selected");
   }
@@ -40,6 +41,8 @@ function submitCsv() {
 function parseToEvent(contents) {
   let eventList = [];
   const rows = contents.split("\n");
+  const header=rows[0];
+  if(validateHeader(header,"events")){return false;};
   rows.forEach((row) => {
     const columns = row.split(",");
     const event = new Event(columns[0], columns[1], columns[2], columns[3]);
@@ -53,12 +56,38 @@ function parseToEvent(contents) {
 function parseToTask(contents) {
   let taskList = [];
   const rows = contents.split("\n");
+  const header=rows[0];
+  if(validateHeader(header,"tasks")){return false;};
   rows.forEach((row) => {
     const columns = row.split(",");
     const event = new Task(columns[0], columns[1]);
     taskList.push(event);
   });
-  console.log(eventList);
+  console.log(taskList);
   localStorage.setItem("tasks", taskList);
   return taskList;
+}
+
+function validateHeader(header,type){
+    console.log(header);
+    if(type=='events'){
+       if(header.trim()=='eventid,eventname,start_date,end_date')
+       {
+               alert("Correct row");
+       }
+       else{
+        alert("Incorrect event headers");
+        return true;
+       }
+    }
+    else if(type=='tasks'){
+        if(header.trim()=='eventid,task_name')
+            {
+                    alert("Correct row");
+            }
+            else{
+             alert("Incorrect task headers");
+             return true;
+            }
+    }
 }
