41d94f66dea50d8a1a156e68fe4ab893bef1cb48
sreedev <xreedev@gmail.com>
2024-07-24 18:40:18 +0530

    final commit day 1

--- script.js
+++ script.js
@@ -24,13 +24,9 @@ function submitCsv() {
     reader.onload = function (e) {
       const contents = e.target.result;
       if (csvType == "events") {
-        if (parseToEvent(contents)) {
-          alert("Events added successfully");
-        }
+        if(parseToEvent(contents)){alert("Events added successfully")};
       } else {
-        if (parseToTask(contents)) {
-          alert("Tasks added successfully");
-        }
+        if(parseToTask(contents)){alert("Tasks added successfully")};
       }
     };
 
@@ -45,10 +41,8 @@ function submitCsv() {
 function parseToEvent(contents) {
   let eventList = [];
   const rows = contents.split("\n");
-  const header = rows[0];
-  if (validateHeader(header, "events")) {
-    return false;
-  }
+  const header=rows[0];
+  if(validateHeader(header,"events")){return false;};
   rows.forEach((row) => {
     const columns = row.split(",");
     const event = new Event(columns[0], columns[1], columns[2], columns[3]);
@@ -62,10 +56,8 @@ function parseToEvent(contents) {
 function parseToTask(contents) {
   let taskList = [];
   const rows = contents.split("\n");
-  const header = rows[0];
-  if (validateHeader(header, "tasks")) {
-    return false;
-  }
+  const header=rows[0];
+  if(validateHeader(header,"tasks")){return false;};
   rows.forEach((row) => {
     const columns = row.split(",");
     const event = new Task(columns[0], columns[1]);
@@ -76,21 +68,26 @@ function parseToTask(contents) {
   return taskList;
 }
 
-function validateHeader(header, type) {
-  console.log(header);
-  if (type == "events") {
-    if (header.trim() == "eventid,eventname,start_date,end_date") {
-      alert("Correct row");
-    } else {
-      alert("Incorrect event headers");
-      return true;
+function validateHeader(header,type){
+    console.log(header);
+    if(type=='events'){
+       if(header.trim()=='eventid,eventname,start_date,end_date')
+       {
+               alert("Correct row");
+       }
+       else{
+        alert("Incorrect event headers");
+        return true;
+       }
     }
-  } else if (type == "tasks") {
-    if (header.trim() == "eventid,task_name") {
-      alert("Correct row");
-    } else {
-      alert("Incorrect task headers");
-      return true;
+    else if(type=='tasks'){
+        if(header.trim()=='eventid,task_name')
+            {
+                    alert("Correct row");
+            }
+            else{
+             alert("Incorrect task headers");
+             return true;
+            }
     }
-  }
 }
