c84bcbf62051fae37d6116abf76b1f9e48a5c146
sreedev <xreedev@gmail.com>
2024-07-24 16:02:03 +0530

    class aded in js for event

---
+++ script.js
@@ -0,0 +1,10 @@
+class event{
+    constructor(eventId,eventName,startDate,endDate){
+      this.eventName=eventName;
+      this.eventId=eventId;
+      this.startDate=startDate;
+      this.endDate=endDate;
+    }
+}
+
+
